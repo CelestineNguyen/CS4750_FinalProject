da58eba4876aaa0740ba06c286b00a606a17ac12
simranarora <simrann.aroraa@gmail.com>
2025-04-09 17:25:11 -0400

    Created HTML template for book details

--- plot_twist/plotTwist/models.py
+++ plot_twist/plotTwist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class BookDetails(models.Model):
+    book_id = models.IntegerField(primary_key=True)
+    title = models.TextField()
+    isbn = models.TextField()
+    pages = models.IntegerField(null=True)
+    date_published = models.DateField(null=True)
+    description = models.TextField(null=True)
+    average_rating = models.FloatField(null=True)
+    authors = models.TextField
+
+    class Meta:
+        db_table = 'book_details'
+        managed = False #view, not a table
