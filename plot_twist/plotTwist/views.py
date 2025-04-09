63e001cf5d6827b8fd4637dd2d320326b588a9f1
simranarora <simrann.aroraa@gmail.com>
2025-04-09 17:32:15 -0400

    Fixed path for template

--- plot_twist/plotTwist/views.py
+++ plot_twist/plotTwist/views.py
@@ -4,4 +4,4 @@ from .models import BookDetails
 # Create your views here.
 def all_books(request):
     books = BookDetails.objects.all()
-    return render(request, 'books/all_books.html', {'books': books})
+    return render(request, 'plotTwist/all_books.html', {'books': books})
