6154a5bf2856af8eee6ffcbebaf0ecaae887887d
simranarora <simrann.aroraa@gmail.com>
2025-04-09 16:53:28 -0400

    Created URL for all books view

--- plot_twist/plot_twist/urls.py
+++ plot_twist/plot_twist/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from plotTwist import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', views.all_books, name='all_books'),
 ]
