76f3fc36d1065d3232f486fd934cb56f48933ecc
Celestine Nguyen <haw3wk@virginia.edu>
2025-04-09 19:57:18 -0400

    fully implemented login and register

--- plot_twist/plotTwist/views.py
+++ plot_twist/plotTwist/views.py
@@ -4,6 +4,21 @@ from django.contrib.auth import login
 
 # Referenced: https://www.w3schools.com/django/django_views.php
 
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "home.html")
+
+
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto login after registration
+            return redirect("home")  # or wherever you want
+    else:
+        form = UserCreationForm()
+
+    return render(request, "register.html", {"form": form})
