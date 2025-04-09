f6616fdb32cb02e1ecac84df633e2cafaa4a14f7
simranarora <simrann.aroraa@gmail.com>
2025-04-09 16:49:42 -0400

    Created view function for BookDetails model

--- plot_twist/plotTwist/views.py
+++ plot_twist/plotTwist/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import BookDetails
 
 # Create your views here.
+def all_books(request):
+    books = BookDetails.objects.all()
+    return render(request, 'books/all_books.html', {'books': books})
